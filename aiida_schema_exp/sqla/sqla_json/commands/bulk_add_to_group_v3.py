3467d8d00dafddc18d31b7b12f8b8a7f894add62
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2018-10-16 19:05:14 +0200

    Various add to group methods to SQLA (for optimization). Its good to have a look at the associationproxy as an alternative to the db_dbgroup_dbnodes table

---
+++ aiida_schema_exp/sqla/sqla_json/commands/bulk_add_to_group_v3.py
@@ -0,0 +1,65 @@
+import click
+
+
+@click.command()
+def cmd():
+    """Creating a user"""
+    # Importing the engine
+    from sqla_json.sqla_management import engine
+    from sqlalchemy.orm import sessionmaker
+    from sqla_json.models.node import DbNode
+    from sqla_json.models.group import DbGroup
+
+    from sqla_json.commands import DEFAULT_GROUP_NAME
+
+    import time
+    import sys
+
+    # Creating the needed session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Getting the one and only group with that name
+    dbgroup = session.query(DbGroup).filter_by(name=DEFAULT_GROUP_NAME).first()
+
+    if dbgroup is None:
+        print "No group is found, exiting"
+        sys.exit()
+
+    # Find all nodes
+    print "Getting all the nodes"
+    start_time = time.time()
+
+    list_node = list()
+    for dbn in session.query(DbNode).all():
+        list_node.append(dbn)
+
+    print "Found #{} nodes".format(len(list_node))
+
+    # Add the nodes to the group
+    print "Adding the nodes to the group"
+    session.bulk_update_mappings(DbGroup., {'id': dbgroup.id,
+                                           'dbnodes': list_node})
+
+    print "LALLLALALA ", {'id': dbgroup.id, 'dbnodes': list_node}
+    # session.add(dbgroup)
+    # session.commit()
+    end_time = time.time()
+
+    print "Added to group"
+    print "Elapsed time --- {} seconds --- ".format(end_time - start_time)
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    dir_of_curr_file = os.path.dirname(os.path.realpath(__file__))
+    # project folder
+    par_dir = os.path.dirname(dir_of_curr_file)
+    proect_dir = os.path.dirname(par_dir)
+    # adding the project folder to the path
+    sys.path.append(proect_dir)
+
+    # calling the command
+    cmd()
