52eaf724554fce380b6fa5a6300c83e66fa2e223
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2018-11-07 15:18:57 +0100

    More options on update json fields

---
+++ aiida_schema_exp/sqla/sqla_json/commands/bulk_update_json_bulk_update_mappings.py
@@ -0,0 +1,65 @@
+import click
+
+
+@click.command()
+def cmd():
+    """Creating a user"""
+    # Importing the engine
+    from sqla_json.sqla_management import engine
+    from sqlalchemy.orm import sessionmaker
+    from sqla_json.models.node import DbNode
+    from sqlalchemy.orm.attributes import flag_modified
+    from sqla_json import timezone
+    # from sqlalchemy.dialects.postgresql import insert
+    from sqlalchemy.sql.expression import update
+
+    import json
+    import time
+
+    sec = timezone.now().second
+    msec = timezone.now().microsecond
+
+    # Creating the needed session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    print "Finding all the nodes and updating them one by one"
+    start_time = time.time()
+
+    print "Constructing mappings"
+    node_mappings = list()
+    counter=0
+    for nid, in session.query(DbNode.id).all():
+        node_mappings.append({'id': nid,
+                              'attributes': json.dumps(
+                                  ['attr', msec + counter, {'bar': ('baz', 'bulk_update_json_model_sql',
+                                                                    sec + counter, 2)}]),
+                              'attributes': json.dumps(
+                                  ['extra', msec + counter, {'bar': ('baz', 'bulk_update_json_model_sql',
+                                                                    sec + counter, 2)}])})
+        counter += 1
+
+    print "Committing mappings"
+    session.bulk_update_mappings(DbNode, node_mappings)
+    session.commit()
+    end_time = time.time()
+
+    print("Updated the json fields (attributes and extras) "
+          "of {} nodes.".format(counter))
+
+    print "Elapsed time --- {} seconds --- ".format(end_time - start_time)
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    dir_of_curr_file = os.path.dirname(os.path.realpath(__file__))
+    # project folder
+    par_dir = os.path.dirname(dir_of_curr_file)
+    proect_dir = os.path.dirname(par_dir)
+    # adding the project folder to the path
+    sys.path.append(proect_dir)
+
+    # calling the command
+    cmd()
---
+++ aiida_schema_exp/sqla/sqla_json/commands/bulk_update_json_model_sql.py
@@ -0,0 +1,66 @@
+import click
+
+
+@click.command()
+def cmd():
+    """Creating a user"""
+    # Importing the engine
+    from sqla_json.sqla_management import engine
+    from sqlalchemy.orm import sessionmaker
+    from sqla_json.models.node import DbNode
+    from sqlalchemy.orm.attributes import flag_modified
+    from sqla_json import timezone
+    # from sqlalchemy.dialects.postgresql import insert
+    from sqlalchemy.sql.expression import update
+
+    import json
+    import time
+
+    sec = timezone.now().second
+    msec = timezone.now().microsecond
+
+    # Creating the needed session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    print "Finding all the nodes and updating them one by one"
+    counter = 0
+    start_time = time.time()
+
+    # # The following works
+    # session.execute(update(DbNode, values={DbNode.attributes: json.dumps(
+    #         ['attr', msec, {'bar': ('baz', 'bulk_update_json_model_sql',
+    #                                           sec, 2)}])})
+    #                 )
+
+    # And the following too
+    def get_json():
+        get_json.counter += 1
+        return json.dumps(
+            ['attr', msec + get_json.counter, {'bar': ('baz', 'bulk_update_json_model_sql',
+                                                       sec + get_json.counter, 2)}])
+    get_json.counter = 0
+    session.execute(update(DbNode, values={DbNode.attributes:get_json()}))
+
+    session.commit()
+    end_time = time.time()
+
+    print("Updated the json fields (attributes and extras) "
+          "of {} nodes.".format(counter))
+
+    print "Elapsed time --- {} seconds --- ".format(end_time - start_time)
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    dir_of_curr_file = os.path.dirname(os.path.realpath(__file__))
+    # project folder
+    par_dir = os.path.dirname(dir_of_curr_file)
+    proect_dir = os.path.dirname(par_dir)
+    # adding the project folder to the path
+    sys.path.append(proect_dir)
+
+    # calling the command
+    cmd()
